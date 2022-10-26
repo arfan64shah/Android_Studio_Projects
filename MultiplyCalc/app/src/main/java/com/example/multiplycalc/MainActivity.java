07873631a8aa99f57156ab35fe119cde0763ffde
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2022-10-26 13:05:09 +0600

    Added multiplication calculator

--- MultiplyCalc/app/src/main/java/com/example/multiplycalc/MainActivity.java
+++ MultiplyCalc/app/src/main/java/com/example/multiplycalc/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.multiplycalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //multiply
+
+    public void Multiply(View v) {
+        EditText et1 = (EditText) findViewById(R.id.editTextNumber);
+        EditText et2 = (EditText) findViewById(R.id.editTextNumber2);
+        EditText et3 = (EditText) findViewById(R.id.editTextNumber3);
+
+        int num1 = Integer.parseInt(et1.getText().toString());
+        int num2 = Integer.parseInt(et2.getText().toString());
+
+        int result = num1*num2;
+
+        et3.setText("Result = " + result);
     }
 }
